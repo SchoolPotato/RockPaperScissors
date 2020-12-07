e3619f9baecbe7f05c353fe58925d2672ea92c40
Alex <awelto389@west-mec.org>
2020-12-07 12:14:04 -0700

    Rock Paper Scissors works now, just need to fix it so it doesn't keep going after a win or loss

--- JS/main.js
+++ JS/main.js
@@ -35,19 +35,19 @@ function RPMTwoThree(){
 
 
     let button1 = document.createElement("BUTTON");
-    button1.id = "mode1";
+    button1.id = "rock";
     button1.innerHTML = "Rock";
-    button1.setAttribute("onclick", "Rock(); rockPaperScissors();");
+    button1.setAttribute("onclick", "Rock(); rockPaperScissors('Rock', 2);");
 
     let button2 = document.createElement("BUTTON");
-    button2.id = "mode2";
+    button2.id = "paper";
     button2.innerHTML="Paper";
-    button2.setAttribute("onclick", "Paper(); rockPaperScissors();");
+    button2.setAttribute("onclick", "Paper(); rockPaperScissors('Paper', 2);");
 
     let button3 = document.createElement("BUTTON");
-    button3.id = "mode3";
+    button3.id = "scissors";
     button3.innerHTML = "Scissors";
-    button3.setAttribute("onclick", "Scissors(); rockPaperScissors();");
+    button3.setAttribute("onclick", "Scissors(); rockPaperScissors('Scissors', 2);");
     
     body.appendChild(paragraph);
     body.appendChild(button1);
@@ -75,18 +75,52 @@ function Scissors(){
     console.log('scissors');
 }
 
+let playerScore = 0;
+let cpuScore = 0;
 
-function rockPaperScissors(decision){
-    let x = Math.floor(Math.random() * Math.floor(3)) + 1;
-    switch(x){
-        case 1:
-            Rock();
-            break;
-        case 2:
-            Paper();
-            break;
-        case 3:
-            Scissors();
-            break;
+function rockPaperScissors(decision, turns){
+    let cpuDecision = Math.floor(Math.random() * Math.floor(3)) + 1;
+    let body = document.getElementById("main");
+    let para = document.createElement("P");
+    para.id = "WinLoss";
+
+    if(decision == 'Rock' && cpuDecision == 3 || decision == 'Paper' && cpuDecision == 1 || decision == 'Scissors' && cpuDecision == 2){
+        playerScore++
+        para.innerHTML = "The player wins. The scores are now: Player's Score: " + playerScore + ", Computer's Score: " + cpuScore;
+        console.log(cpuScore);
+        console.log(playerScore);
+    } else if (decision == 'Rock' && cpuDecision == 1 || decision == 'Paper' && cpuDecision == 2 || decision == 'Scissors' && cpuDecision == 3){
+        para.innerHTML = "It's a tie. Try again."
+    } else {
+        cpuScore++
+        para.innerHTML = "The computer wins. The scores are now: Player's Score: " + playerScore + ", Computer's Score: " + cpuScore;
+        console.log(cpuScore);
+        console.log(playerScore);
+    }
+    body.appendChild(para);
+
+    if(playerScore <= turns && cpuScore <= turns){
+        switch(turns){
+            case 3:
+                RPMTwoThree();
+                break;
+            case 5:
+                RPMThreeFive();
+                break;
+            case 7:
+                RPMFourSeven();
+                break;
+        }
+
+    }else if(playerScore > cpuScore && playerScore >= turns){
+        let para2 = document.createElement("P");
+        para2.innerHTML = "The player wins the whole match. Victory is yours!";
+        body.appendChild(para2);
+        playerScore = 0;
+        cpuScore = 0;
+    }else if (playerScore < cpuScore && cpuScore >= turns){
+        let para2 = document.createElement("P");
+        para2.innerHTML = "The computer wins the whole match. You've been defeated! Would you like to try again?";
+        body.appendChild(para2);
     }
 }
